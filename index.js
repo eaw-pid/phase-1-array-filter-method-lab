275fbce71f4c3c9f8178aa23d2c6a6190296f78d
Erin Wise <erinwise88@gmail.com>
2023-08-15 11:01:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+/*function filter(collection, cb) {
+    for (const user of collection) {
+        console.log(user);
+    }
+}
+
+*/
+
+function findMatching(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver.toUpperCase() === string.toUpperCase();
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver.startsWith(string);
+    }
+)}
+
+function matchName(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver.name === string;
+    })
+}
